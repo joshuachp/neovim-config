0d0ada1580210cd96e05f682420e126a640efd4d
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-03-19 10:27:06 +0100

    feat(nix): install tree-sitter grammars from nix
    Install tree-sitter grammars from nix instead of using the `:TSUpdate`
    command. This is more reliable and faster without the errors of the
    missing libraries on nix systems.

--- flake.nix
+++ flake.nix
@@ -45,11 +45,17 @@
             };
             config = {
               nixpkgs.overlays = [ neovim-nightly-overlay.overlay ];
-              environment.systemPackages = with pkgs; [
-                (neovim.overrideAttrs (oas: {
-                  wrapRc = false;
-                }))
-              ];
+              programs.neovim = {
+                enable = true;
+                configure = {
+                  customRC = ''
+                    source $XDG_CONFIG_HOME/nvim/init.lua
+                  '';
+                  packages = {
+                    nvim-treesitter = pkgs.vimPlugins.nvim-treesitter.withAllGrammars;
+                  };
+                };
+              };
             };
           };
         default = neovim-config;
--- lua/plugins/syntax.lua
+++ lua/plugins/syntax.lua
@@ -3,7 +3,6 @@ return {
   -- Tree-sitter
   {
     'nvim-treesitter/nvim-treesitter',
-    build = ':TSUpdate',
     config = function()
       require('nvim-treesitter.configs').setup({
         -- ensure_installed = "maintained",
