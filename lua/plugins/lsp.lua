dfc12a92ea5d274eee6e80e312c341962cacaaf6
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-04-03 15:57:49 +0200

    feat(rust): add custom config for specific path

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -32,6 +32,16 @@ return {
         -- these override the defaults set by rust-tools.nvim
         -- see https://github.com/neovim/nvim-lspconfig/blob/master/doc/server_configurations.md#rust_analyzer
         server = {
+          on_init = function(client)
+            local path = client.workspace_folders[1].name
+
+            if path == '/home/joshuachp/share/repos/seco/edgehog-device-runtime-wrapped/edgehog-device-runtime' then
+              client.config.settings['rust-analyzer'].cargo.features = { 'docker' }
+            end
+
+            client.notify('workspace/didChangeConfiguration', { settings = client.config.settings })
+            return true
+          end,
           on_attach = lsp.on_attach,
           capabilities = lsp.default_capabilities(),
           settings = {
