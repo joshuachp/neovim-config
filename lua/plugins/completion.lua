8e3cdb165ad47264dd802d10d95983d6c8627ddf
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-06-06 12:24:43 +0200

    feat(cmp): add copilot support for cmp

--- lua/plugins/completion.lua
+++ lua/plugins/completion.lua
@@ -40,4 +40,12 @@ return {
       })
     end,
   },
+  {
+    'zbirenbaum/copilot-cmp',
+    config = function()
+      require('copilot_cmp').setup()
+
+      vim.api.nvim_set_hl(0, 'CmpItemKindCopilot', { fg = '#6CC644' })
+    end,
+  },
 }
