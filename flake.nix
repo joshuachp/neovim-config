bba7bee1974a5de2b3d60051684a8e69848ca8f8
Joshua Chapman <joshua.chapman@joshuachp.dev>
2023-09-13 15:21:31 +0200

    feat(treesitter): make import and generate the treesitter
    Make treesitter a flake input so that the parsers are easier to update
    with the lock file. Remove the integrated treesitter parsers from the
    RTP path so they don't conflict with the one in nvim-treesitter.

--- flake.nix
+++ flake.nix
@@ -12,22 +12,32 @@
     };
     # Utils for the mapping the system variable etc.
     flake-utils.url = "github:numtide/flake-utils";
+    # nvim-treesitter to have access to the lock file
+    nvimTreesitter = {
+      url = "github:nvim-treesitter/nvim-treesitter";
+      flake = false;
+    };
   };
   outputs =
     { self
     , nixpkgs
     , flake-utils
     , neovim-nightly-overlay
+    , nvimTreesitter
     }@inputs: {
       nixosModules = import ./nix/nixosModules.nix inputs;
       homeManagerModules = import ./nix/homeManagerModules.nix inputs;
     } // flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs { inherit system; };
+      nvim-treesitter = callPackage ./nix/treesitter.nix { inherit nvimTreesitter; };
       inherit (pkgs) callPackage;
     in
     {
       checks.check = callPackage ./nix/check.nix { };
       devShells.default = callPackage ./nix/shell.nix { };
-    });
+      packages.nvim-treesitter = nvim-treesitter;
+    }) // {
+      overlays.nvim-treesitter = import ./nix/treesitterOverlay.nix nvimTreesitter;
+    };
 }
---
+++ nix/treesitterOverlay.nix
@@ -0,0 +1,14 @@
+nvimTreesitter:
+final: prev:
+{
+  vimPlugins = prev.vimPlugins.extend (final': prev':
+    let
+      nvim-treesitter = prev.callPackage ./treesitter.nix {
+        vimPlugins = prev';
+        inherit nvimTreesitter;
+      };
+    in
+    {
+      inherit nvim-treesitter;
+    });
+}
