6c700663aafdb8a29bec4ce12361184ce75494d2
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-04-11 11:43:50 +0200

    feat(theme): remove foreground color from diff

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,24 +1,29 @@
 --- Color theme and highlight configuration
 return {
-  'ellisonleao/gruvbox.nvim',
-  priority = 1000,
-  config = function()
-    local gruvbox = require('gruvbox')
-    gruvbox.setup({
-      contrast = 'hard',
-      overrides = {
-        NormalFloat = { bg = gruvbox.palette.dark0_hard },
-      },
-    })
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = function()
+      local gruvbox = require('gruvbox')
+      gruvbox.setup({
+        contrast = 'hard',
+        overrides = {
+          NormalFloat = { bg = gruvbox.palette.dark0_hard },
+          DiffText = { bg = gruvbox.palette.faded_yellow },
+        },
+      })
 
-    vim.cmd.colorscheme('gruvbox')
+      vim.cmd.colorscheme('gruvbox')
 
-    vim.api.nvim_set_hl(0, 'LspReferenceText', { link = 'SignColumn' })
-    vim.api.nvim_set_hl(0, 'LspReferenceRead', { link = 'SignColumn' })
-    vim.api.nvim_set_hl(0, 'LspReferenceWrite', { link = 'SignColumn' })
+      vim.api.nvim_set_hl(0, 'DiffText', { bg = gruvbox.palette.faded_yellow })
 
-    vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GruvboxGreenSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GruvboxRedSign' })
-    vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GruvboxAquaSign' })
-  end,
+      vim.api.nvim_set_hl(0, 'LspReferenceText', { link = 'SignColumn' })
+      vim.api.nvim_set_hl(0, 'LspReferenceRead', { link = 'SignColumn' })
+      vim.api.nvim_set_hl(0, 'LspReferenceWrite', { link = 'SignColumn' })
+
+      vim.api.nvim_set_hl(0, 'GitSignsAdd', { link = 'GruvboxGreenSign' })
+      vim.api.nvim_set_hl(0, 'GitSignsDelete', { link = 'GruvboxRedSign' })
+      vim.api.nvim_set_hl(0, 'GitSignsChange', { link = 'GruvboxAquaSign' })
+    end,
+  },
 }
