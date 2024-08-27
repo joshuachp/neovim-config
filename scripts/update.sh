8fa7d912789ec50f11f5d33ca577325d07e1f596
Joshua Chapman <joshua.chapman@joshuachp.dev>
2024-08-27 11:45:29 +0200

    feat(update): improve the update script

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,6 +2,8 @@
 
 set -exEuo pipefail
 
+changed=''
+
 is_git_clean() {
     if [[ -z "$(git status -s)" ]]; then
         return
@@ -19,11 +21,25 @@ git pull
 
 git switch --create chore/update
 
+pre-commit autoupdate
+
+if ! git diff --quiet .pre-commit-config.yaml; then
+    git add .pre-commit-config.yaml
+    git commit -m 'chore(pre-commit): update pre-commit-config.yaml'
+
+    changed+=', pre-commit-config.yaml'
+fi
+
+is_git_clean
+
 nix flake update
 
-git add flake.lock
+if ! git diff --quiet flake.lock; then
+    git add flake.lock
+    git commit -m 'chore(nix): update flake.lock'
 
-git commit -m 'chore(nix): update flake.lock'
+    changed+=', flake.lock'
+fi
 
 is_git_clean
 
@@ -31,20 +47,35 @@ nvim --headless "+Lazy! sync" +qa
 
 pre-commit run --files lazy-lock.json
 
-git add lazy-lock.json
+if ! git diff --quiet lazy-lock.json; then
+    git add lazy-lock.json
+    git commit -m 'chore(lazy): update lazy-lock.json'
 
-git commit -m 'chore(lazy): update lazy-lock.json'
+    changed+=', lazy-lock.json'
+fi
 
 is_git_clean
 
 nix run .#update-parsers >./nix/treesitter-sources.nix
 
-git add nix/treesitter-sources.nix
+if ! git diff --quiet nix/treesitter-sources.nix; then
+    git add nix/treesitter-sources.nix
+    git commit -m 'chore(treesitter): update parsers sources'
 
-git commit -m 'chore(treesitter): update parsers sources'
+    changed+=', tree-sitter parsers'
+fi
+
+changed="${changed#, }"
+
+if [ -z "$changed" ]; then
+    echo 'nothing changed'
+    exit 0
+fi
 
 git push
 
 gh pr create \
     --title 'chore(deps): update dependencies' \
-    --body 'Update nix flake.lock, lazy-lock.json and tree-sitter parsers.'
+    --body "Update $changed."
+
+gh pr view --web
